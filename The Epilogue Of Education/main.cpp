2157a6332b0882849074da14e31e8e2e3fad4962
Amruth Pillai <im.amruth@gmail.com>
2017-03-19 18:49:37 +0530

    added scene 2, 325 lines milestone

--- The Epilogue Of Education/main.cpp
+++ The Epilogue Of Education/main.cpp
@@ -16,7 +16,11 @@ intro_next_text_appear, summary_next_text_appear,
 para1_fade, para2_fade, para3_fade, para4_fade;
 
 // Function to Print Text
-void print(char *string, GLfloat r, GLfloat g, GLfloat b, GLfloat a, GLint x, GLint y, GLfloat w, GLfloat h, GLfloat strokeSize)
+void print(char *string,
+	GLfloat r, GLfloat g, GLfloat b, GLfloat a,
+	GLint x, GLint y,
+	GLfloat w, GLfloat h,
+	GLfloat strokeSize)
 {
 	glPushMatrix();
 
@@ -32,42 +36,163 @@ void print(char *string, GLfloat r, GLfloat g, GLfloat b, GLfloat a, GLint x, GL
 	glPopMatrix();
 }
 
-// Scene 0 - Introduction Screen
+/*
+* Scene 0 - Introduction Screen
+*/
 void introduction(void)
 {
+	// Background
 	glClearColor(0.05, 0.05, 0.05, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	print("The Epilogue of Education", 1, 1, 1, title_fade, 280, 380, .3, .3, 2);
+	// Story Title
+	print("The Epilogue of Education",
+		1, 1, 1, title_fade, 280, 380, .3, .3, 2);
 
-	print("by Raja Rajan A.", 1, 1, 1, 1, 790, 345, .15, .15, 1);
-	print("(1DS15CS417)", 1, 1, 1, 1, 840, 320, .1, .1, 1);
+	print("by Raja Rajan A.",
+		1, 1, 1, 1, 790, 345, .15, .15, 1);
+	print("(1DS15CS417)",
+		1, 1, 1, 1, 840, 320, .1, .1, 1);
 
-	print("Press N to read the story...", 1, 1, 1, intro_next_text_appear, 560, 10, .09, .09, 1);
+	print("Press N to read the story...",
+		1, 1, 1, intro_next_text_appear, 560, 10, .09, .09, 1);
 }
 
-// Scene 1 - Summary Screen
+/*
+* Scene 1 - Summary Screen
+*/
 void summary(void)
 {
+	// Background
 	glClearColor(0.05, 0.05, 0.05, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	// Summary Title
 	print("Summary", 1, 1, 1, 1, 75, 650, .3, .3, 1.5);
 
-	print("As children, we are taught to follow, not to lead. We are taught to learn, not to explore.", 1, 1, 1, para1_fade, 75, 575, .125, .125, 1.25);
-	print("We are taught to by heart, not to understand.", 1, 1, 1, para1_fade, 75, 550, .125, .125, 1.25);
+	print("As children, we are taught to follow, not to lead. We are taught to learn, not to explore.",
+		1, 1, 1, para1_fade, 75, 575, .125, .125, 1.25);
+	print("We are taught to by heart, not to understand.",
+		1, 1, 1, para1_fade, 75, 550, .125, .125, 1.25);
 
-	print("When we grow up, things don’t change. We are given a meagre choice of either choosing", 1, 1, 1, para2_fade, 75, 475, .125, .125, 1.25);
-	print("to be a Doctor or an Engineer, and the rest of the options are at bleak focus. ", 1, 1, 1, para2_fade, 75, 450, .125, .125, 1.25);
+	print("When we grow up, things don’t change. We are given a meagre choice of either choosing",
+		1, 1, 1, para2_fade, 75, 475, .125, .125, 1.25);
+	print("to be a Doctor or an Engineer, and the rest of the options are at bleak focus. ",
+		1, 1, 1, para2_fade, 75, 450, .125, .125, 1.25);
 
-	print("Choosing to be a doctor is a rich man’s dream.", 1, 1, 1, para3_fade, 75, 375, .125, .125, 1.25);
-	print("Ending up as an engineer has become a curse. ", 1, 1, 1, para3_fade, 75, 350, .125, .125, 1.25);
+	print("Choosing to be a doctor is a rich man’s dream.",
+		1, 1, 1, para3_fade, 75, 375, .125, .125, 1.25);
+	print("Ending up as an engineer has become a curse. ",
+		1, 1, 1, para3_fade, 75, 350, .125, .125, 1.25);
 
-	print("Engineering is a field to invent and innovate, not to follow orders from higher officials.", 1, 1, 1, para4_fade, 75, 275, .125, .125, 1.25);
-	print("We might as well study the lives of Hitler’s Soldiers if we were to do so", 1, 1, 1, para4_fade, 75, 250, .125, .125, 1.25);
-	print("for the rest of out lives.", 1, 1, 1, para4_fade, 75, 225, .125, .125, 1.25);
+	print("Engineering is a field to invent and innovate, not to follow orders from higher officials.",
+		1, 1, 1, para4_fade, 75, 275, .125, .125, 1.25);
+	print("We might as well study the lives of Hitler’s Soldiers if we were to do so",
+		1, 1, 1, para4_fade, 75, 250, .125, .125, 1.25);
+	print("for the rest of out lives.",
+		1, 1, 1, para4_fade, 75, 225, .125, .125, 1.25);
 
-	print("Press N to move on...", 1, 1, 1, summary_next_text_appear, 580, 10, .09, .09, 1);
+	print("Press N to move on...",
+		1, 1, 1, summary_next_text_appear, 580, 10, .09, .09, 1);
+}
+
+/*
+* Scene 2 - Kindergarten
+*/
+void kindergarten(void)
+{
+	// Background
+	glClearColor(0.05, 0.05, 0.05, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Ceiling
+	glBegin(GL_POLYGON);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(0, 800);
+	glColor3ub(251, 242, 233); // Light Brown
+	glVertex2f(200, 700);
+	glColor3ub(251, 242, 233); // Light Brown
+	glVertex2f(1400, 700);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(1400, 800);
+	glEnd();
+
+	// Border for Ceiling
+	glLineWidth(2);
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(0, 0, 0);
+	glVertex2f(0, 800);
+	glVertex2f(200, 700);
+	glVertex2f(1400, 700);
+	glVertex2f(1400, 800);
+	glEnd();
+	glLineWidth(1);
+
+	// Side Wall
+	glBegin(GL_POLYGON);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(0, 800);
+	glColor3ub(251, 242, 233); // Light Brown
+	glVertex2f(200, 700);
+	glColor3ub(251, 242, 233); // Light Brown
+	glVertex2f(200, 175);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(0, 75);
+	glEnd();
+
+	// Border for Side Wall
+	glLineWidth(2);
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(0, 0, 0);
+	glVertex2f(0, 800);
+	glVertex2f(200, 700);
+	glVertex2f(200, 175);
+	glVertex2f(0, 75);
+	glEnd();
+	glLineWidth(1);
+
+	// Center Wall
+	glBegin(GL_POLYGON);
+	glColor3ub(251, 242, 233); // Light Brown
+	glVertex2f(200, 700);
+	glColor3ub(251, 242, 233); // Light Brown
+	glVertex2f(1400, 700);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(1400, 175);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(200, 175);
+	glEnd();
+
+	// Border for Center Wall
+	glLineWidth(2);
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(0, 0, 0);
+	glVertex2f(200, 700);
+	glVertex2f(1400, 700);
+	glVertex2f(1400, 175);
+	glVertex2f(200, 175);
+	glEnd();
+	glLineWidth(1);
+
+	// Floor
+	glBegin(GL_POLYGON);
+	glColor3ub(166, 148, 124); // Dark Brown
+	glVertex2f(200, 175);
+	glVertex2f(1400, 175);
+	glVertex2f(1400, 75);
+	glVertex2f(0, 75);
+	glEnd();
+
+	// Border for Floor
+	glLineWidth(2);
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(0, 0, 0);
+	glVertex2f(200, 175);
+	glVertex2f(1400, 175);
+	glVertex2f(1400, 75);
+	glVertex2f(0, 75);
+	glEnd();
+	glLineWidth(1);
 }
 
 // Function to Render Scene
@@ -80,6 +205,9 @@ void renderScene(void)
 		break;
 	case 1:
 		summary();
+		break;
+	case 2:
+		kindergarten();
 		break;
 	default:
 		break;
@@ -98,7 +226,6 @@ void mouseClick(int button, int state, int x, int y)
 
 // Animator Updation Function
 void update(int) {
-
 	// Introduction
 	if (SCENE_ID == 0) {
 		if (title_fade < 1)
@@ -122,6 +249,11 @@ void update(int) {
 						para4_fade += .005;
 					else
 						summary_next_text_appear = 1;
+	}
+
+	// Kindergarten
+	if (SCENE_ID == 2) {
+
 	}
 
 	// Recalls the Display Function
@@ -157,17 +289,12 @@ void keyPress(unsigned char key, int x, int y)
 	glutPostRedisplay();
 }
 
-// Idle Action
-void idleFunc() {
-	glutPostRedisplay();
-}
-
 // Function to Initialize Screen
 void initializeScreen(void)
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, 1366, 0, 768);
+	gluOrtho2D(0, 1400, 0, 800);
 }
 
 // Main Function
@@ -176,7 +303,7 @@ int main(int argc, char **argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(1366, 768);
+	glutInitWindowSize(1400, 800);
 	glutCreateWindow("OpenGL First Window");
 
 	// Enables Transparency
@@ -187,7 +314,6 @@ int main(int argc, char **argv)
 
 	glutKeyboardFunc(keyPress);
 	glutMouseFunc(mouseClick);
-	glutIdleFunc(idleFunc);
 
 	initializeScreen();
 
