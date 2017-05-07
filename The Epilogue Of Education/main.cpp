ab46fda45bb42628fd8ebc611f91d85ded96022a
Amruth Pillai <im.amruth@gmail.com>
2017-05-08 01:10:05 +0530

    almost reaching the end

--- The Epilogue Of Education/main.cpp
+++ The Epilogue Of Education/main.cpp
@@ -13,7 +13,7 @@
 #define PI 3.141592653589
 
 // ID to detect which scene the story is in.
-int SCENE_ID = 9;
+int SCENE_ID;
 
 // Variables for Animator Faders
 GLfloat title_fade,
@@ -25,7 +25,8 @@ GLfloat title_fade,
 	ps_subtitle_1_appear, ps_subtitle_2_appear,
 	hs_chap_fade, hs_title_fade,
 	hs_subtitle_1_appear,
-	puc_chap_fade, puc_title_fade;
+	puc_chap_fade, puc_title_fade,
+	puc_subtitle_1_fade;
 
 // Variables for Translation Animators
 GLfloat trans_x_chap1, trans_x_title1,
@@ -56,6 +57,7 @@ void print(char *string,
 	glColor4f(r, g, b, a);
 	glTranslatef(x, y, 0);
 	glScalef(w, h, 0);
+
 	glPointSize(strokeSize);
 	glLineWidth(strokeSize);
 
@@ -63,6 +65,9 @@ void print(char *string,
 		glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN, *string++);
 
 	glPopMatrix();
+
+	glLineWidth(1);
+	glPointSize(1);
 }
 
 // Function to Draw Circle
@@ -1656,10 +1661,12 @@ void HS_drawStars() {
 	}
 }
 
-void HS_drawSchoolBoy(GLfloat tx, GLfloat ty) {
-		glPushMatrix();
+void drawSchoolBoy(GLfloat tx, GLfloat ty,
+	GLfloat sx, GLfloat sy,
+	GLfloat shirt_r, GLfloat shirt_g, GLfloat shirt_b) {
+	glPushMatrix();
 
-	glScalef(.5, .5, 0);
+	glScalef(sx, sy, 0);
 	glTranslatef(tx, ty, 0);
 
 	// Shoes
@@ -1689,7 +1696,7 @@ void HS_drawSchoolBoy(GLfloat tx, GLfloat ty) {
 
 	// Shirt
 	glBegin(GL_POLYGON);
-	glColor3ub(255, 0, 0);
+	glColor3ub(shirt_r, shirt_g, shirt_b);
 	glVertex2f(1155, 190);
 	glVertex2f(1170, 190);
 	glVertex2f(1170, 260);
@@ -1767,7 +1774,10 @@ void highSchool() {
 
 	HS_drawLights();
 	HS_drawStars();
-	HS_drawSchoolBoy(schoolboy_x, schoolboy_y);
+
+	drawSchoolBoy(schoolboy_x, schoolboy_y,
+		.5, .5,
+		255, 0, 0);
 
 	print("Home -> School -> Tuition -> Home -> School -> Tuition -> Home -> School -> Tuition",
 		1, 1, 1, hs_subtitle_1_appear, 80, 30, .14, .14, 1);
@@ -1792,6 +1802,7 @@ void PUC_drawFloor() {
 	glColor3ub(225, 215, 205);
 	glVertex2f(0, 75);
 	glVertex2f(1400, 75);
+	glColor3ub(210, 193, 179);
 	glVertex2f(775, 200);
 	glVertex2f(625, 200);
 	glEnd();
@@ -1809,7 +1820,7 @@ void PUC_drawFloor() {
 void PUC_drawCenterWall() {
 	// Center Wall
 	glBegin(GL_POLYGON);
-	glColor3ub(225, 215, 205);
+	glColor3ub(230, 230, 230);
 	glVertex2f(625, 200);
 	glVertex2f(775, 200);
 	glVertex2f(775, 450);
@@ -1827,7 +1838,7 @@ void PUC_drawCenterWall() {
 
 	// Center Wall Strip
 	glBegin(GL_POLYGON);
-	glColor3ub(50, 50, 50);
+	glColor3ub(0, 130, 255);
 	glVertex2f(625, 200);
 	glVertex2f(775, 200);
 	glVertex2f(775, 207.81);
@@ -1847,9 +1858,10 @@ void PUC_drawCenterWall() {
 void PUC_drawCeiling() {
 	// Ceiling
 	glBegin(GL_POLYGON);
-	glColor3ub(238, 229, 219);
+	glColor3ub(230, 230, 230);
 	glVertex2f(625, 450);
 	glVertex2f(775, 450);
+	glColor3ub(240, 240, 240);
 	glVertex2f(1400, 800);
 	glVertex2f(0, 800);
 	glEnd();
@@ -1867,7 +1879,7 @@ void PUC_drawCeiling() {
 void PUC_drawLeftWall() {
 	// Left Wall
 	glBegin(GL_POLYGON);
-	glColor3ub(210, 193, 179);
+	glColor3ub(240, 240, 240);
 	glVertex2f(0, 75);
 	glVertex2f(625, 200);
 	glVertex2f(625, 450);
@@ -1885,7 +1897,7 @@ void PUC_drawLeftWall() {
 
 	// Left Wall Bottom Strip
 	glBegin(GL_POLYGON);
-	glColor3ub(50, 50, 50);
+	glColor3ub(0, 130, 255);
 	glVertex2f(0, 75);
 	glVertex2f(625, 200);
 	glVertex2f(625, 207.81);
@@ -1905,7 +1917,7 @@ void PUC_drawLeftWall() {
 void PUC_drawRightWall() {
 	// Right Wall
 	glBegin(GL_POLYGON);
-	glColor3ub(210, 193, 179);
+	glColor3ub(240, 240, 240);
 	glVertex2f(775, 200);
 	glVertex2f(1400, 75);
 	glVertex2f(1400, 800);
@@ -1923,7 +1935,7 @@ void PUC_drawRightWall() {
 
 	// Right Wall Bottom Strip
 	glBegin(GL_POLYGON);
-	glColor3ub(50, 50, 50);
+	glColor3ub(0, 130, 255);
 	glVertex2f(1400, 75);
 	glVertex2f(1400, 100);
 	glVertex2f(775, 207.81);
@@ -1994,6 +2006,38 @@ void PUC_drawLeftDoors() {
 	glVertex2f(150, 548);
 	glEnd();
 
+	// PCMB
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 255, 255);
+	glVertex2f(140, 520);
+	glVertex2f(140, 490);
+	glVertex2f(250, 490);
+	glVertex2f(250, 520);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(140, 520);
+	glVertex2f(140, 490);
+	glVertex2f(250, 490);
+	glVertex2f(250, 520);
+	glEnd();
+
+	drawCircle(138, 510, 100, 100, 100, 1, 1, 3);
+	drawCircle(142, 510, 100, 100, 100, 1, 1, 3);
+
+	drawCircle(138, 499, 100, 100, 100, 1, 1, 3);
+	drawCircle(142, 499, 100, 100, 100, 1, 1, 3);
+
+	print("P C M B",
+		0, 0, 0, 1, 160, 500, .1, .1, 2);
+
+	// Door Knob
+	drawCircle(320, 300,
+		223, 189, 31, // Goldish Yellow
+		1, 1,
+		5);
+
 	// Second Door
 	glBegin(GL_POLYGON);
 	glColor3ub(210, 210, 210);
@@ -2029,6 +2073,45 @@ void PUC_drawLeftDoors() {
 	glVertex2f(540, 403);
 	glVertex2f(450, 447);
 	glEnd();
+
+	// CEBA
+	glPushMatrix();
+
+	glTranslatef(325, 10, 0);
+	glScalef(.8, .8, 1);
+
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 255, 255);
+	glVertex2f(140, 520);
+	glVertex2f(140, 490);
+	glVertex2f(250, 490);
+	glVertex2f(250, 520);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(140, 520);
+	glVertex2f(140, 490);
+	glVertex2f(250, 490);
+	glVertex2f(250, 520);
+	glEnd();
+
+	drawCircle(138, 510, 100, 100, 100, 1, 1, 3);
+	drawCircle(142, 510, 100, 100, 100, 1, 1, 3);
+
+	drawCircle(138, 499, 100, 100, 100, 1, 1, 3);
+	drawCircle(142, 499, 100, 100, 100, 1, 1, 3);
+
+	print("C E B A",
+		0, 0, 0, 1, 160, 500, .1, .1, 2);
+
+	glPopMatrix();
+
+	// Door Knob
+	drawCircle(530, 300,
+		223, 189, 31, // Goldish Yellow
+		1, 1,
+		3);
 }
 
 void PUC_drawRightDoors() {
@@ -2068,6 +2151,38 @@ void PUC_drawRightDoors() {
 	glVertex2f(1250, 548);
 	glEnd();
 
+	// PCMC
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 255, 255);
+	glVertex2f(1260, 520);
+	glVertex2f(1260, 490);
+	glVertex2f(1150, 490);
+	glVertex2f(1150, 520);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(1260, 520);
+	glVertex2f(1260, 490);
+	glVertex2f(1150, 490);
+	glVertex2f(1150, 520);
+	glEnd();
+
+	drawCircle(1258, 510, 100, 100, 100, 1, 1, 3);
+	drawCircle(1262, 510, 100, 100, 100, 1, 1, 3);
+
+	drawCircle(1258, 499, 100, 100, 100, 1, 1, 3);
+	drawCircle(1262, 499, 100, 100, 100, 1, 1, 3);
+
+	print("P C M C",
+		0, 0, 0, 1, 1165, 500, .1, .1, 2);
+
+	// Door Knob
+	drawCircle(1080, 300,
+		223, 189, 31, // Goldish Yellow
+		1, 1,
+		5);
+
 	// Second Door
 	glBegin(GL_POLYGON);
 	glColor3ub(210, 210, 210);
@@ -2103,7 +2218,94 @@ void PUC_drawRightDoors() {
 	glVertex2f(860, 403);
 	glVertex2f(950, 447);
 	glEnd();
+
+	// PCME
+	glPushMatrix();
+
+	glTranslatef(-45, 10, 0);
+	glScalef(.8, .8, 1);
+
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 255, 255);
+	glVertex2f(1260, 520);
+	glVertex2f(1260, 490);
+	glVertex2f(1150, 490);
+	glVertex2f(1150, 520);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(1260, 520);
+	glVertex2f(1260, 490);
+	glVertex2f(1150, 490);
+	glVertex2f(1150, 520);
+	glEnd();
+
+	drawCircle(1258, 510, 100, 100, 100, 1, 1, 3);
+	drawCircle(1262, 510, 100, 100, 100, 1, 1, 3);
+
+	drawCircle(1258, 499, 100, 100, 100, 1, 1, 3);
+	drawCircle(1262, 499, 100, 100, 100, 1, 1, 3);
+
+	print("P C M E",
+		0, 0, 0, 1, 1165, 500, .1, .1, 2);
+
+	glPopMatrix();
+
+	// Door Knob
+	drawCircle(870, 300,
+		223, 189, 31, // Goldish Yellow
+		1, 1,
+		3);
 }
+
+void PUC_drawBoard(){
+
+	//Board Hanging Line
+	glLineWidth(4);
+	glBegin(GL_LINES);
+	glColor3ub(20, 20, 20);
+	glVertex2f(550, 800);
+	glVertex2f(550, 750);
+	glVertex2f(850, 800);
+	glVertex2f(850, 750);
+	glEnd();
+	glLineWidth(1);
+
+	// Board Border
+	glBegin(GL_POLYGON);
+	glColor3ub(20, 20, 20);
+	glVertex2f(450, 750);
+	glVertex2f(450, 600);
+	glVertex2f(950, 600);
+	glVertex2f(950, 750);
+	glEnd();
+
+	//Board
+	glBegin(GL_POLYGON);
+	glColor3ub(0, 131, 255);
+	glVertex2f(452, 748);
+	glColor3ub(0, 104, 204);
+	glVertex2f(452, 602);
+	glVertex2f(948, 602);
+	glColor3ub(0, 131, 255);
+	glVertex2f(948, 748);
+	glEnd();
+
+	print("H A R V A R D",
+		1, 1, 1, 1, 565, 690, .2, .2, 4);
+
+	print("Pre University College",
+		1, 1, 1, 1, 530, 660, .15, .15, 2);
+
+	print("BENGALURU",
+		1, 1, 1, 1, 650, 630, .12, .12, 1);
+}
+
+// Variables for PUC Animation
+bool puc_begin_anim, puc_end_anim;
+GLfloat trans_x_sb1 = -630, trans_y_sb1 = -50,
+	trans_x_sb2 = -1630, trans_y_sb2 = -50;
 
 // Pre University College
 void PUC() {
@@ -2122,6 +2324,20 @@ void PUC() {
 
 	PUC_drawLeftDoors();
 	PUC_drawRightDoors();
+	PUC_drawBoard();
+
+	// Red Boy
+	drawSchoolBoy(trans_x_sb1, trans_y_sb1,
+		1.4, 1.4,
+		255, 0, 0);
+
+	// Green Boy
+	drawSchoolBoy(trans_x_sb2, trans_y_sb2,
+		-1.4, 1.4,
+		0, 255, 0);
+
+	print("Some students choose PCMB, some choose PCMC, and some...",
+		1, 1, 1, puc_subtitle_1_fade, 300, 30, .14, .14, 1);
 }
 
 // Function to Render Scene
@@ -2408,7 +2624,38 @@ void update(int) {
 
 	// Pre University College
 	if (SCENE_ID == 9) {
+		if (puc_subtitle_1_fade < 1)
+			puc_subtitle_1_fade += 0.005;
 
+		if (!puc_begin_anim) {
+			delay(2);
+			puc_begin_anim = true;
+		}
+
+		if (!puc_end_anim) {
+			if (trans_x_sb1 >= -930)
+				trans_x_sb1 -= 1;
+			if (trans_y_sb1 <= -20)
+				trans_y_sb1 += .1;
+
+			if (trans_x_sb2 >= -1930)
+				trans_x_sb2 -= 1;
+			if (trans_y_sb2 <= -20)
+				trans_y_sb2 += .1;
+
+			if (trans_y_sb1 >= -20 && trans_y_sb2 >= -20)
+				puc_end_anim = true;
+		}
+
+		if (puc_end_anim) {
+			delay(1);
+
+			if (trans_y_sb1 <= 5000)
+				trans_y_sb1 += 5000;
+
+			if (trans_y_sb2 <= 5000)
+				trans_y_sb2 += 5000;
+		}
 	}
 
 	// Recalls the Display Function
