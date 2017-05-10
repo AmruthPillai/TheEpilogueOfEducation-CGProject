55ad39aa1fc4d65b9318bf65b17c55738ba237db
Amruth Pillai <im.amruth@gmail.com>
2017-05-11 02:30:31 +0530

    workplace woes

--- The Epilogue Of Education/main.cpp
+++ The Epilogue Of Education/main.cpp
@@ -14,7 +14,7 @@
 #define PI 3.141592653589
 
 // ID to detect which scene the story is in.
-int SCENE_ID = 11;
+int SCENE_ID = 13;
 
 // Variables for Animator Faders
 GLfloat title_fade,
@@ -29,7 +29,8 @@ GLfloat title_fade,
 	puc_chap_fade, puc_title_fade,
 	puc_subtitle_1_fade,
 	eng_chap_fade, eng_title_fade,
-	ec_subtitle_1_fade;
+	ec_subtitle_1_fade,
+	wp_chap_fade, wp_title_fade;
 
 // Variables for Translation Animators
 GLfloat trans_x_chap1, trans_x_title1,
@@ -42,7 +43,8 @@ GLfloat trans_x_chap1, trans_x_title1,
 	schoolboy_x = 1150, schoolboy_y = 5220,
 	trans_x_sb1 = -630, trans_y_sb1 = -50, trans_x_sb2 = -1630, trans_y_sb2 = -50,
 	trans_x_chap5, trans_x_title5,
-	trans_x_ec_sb1 = 850, trans_x_ec_sb2 = -1000, trans_y_ec_sb = 40, trans_x_ec_gb = -180, trans_y_ec_gb = 1040;
+	trans_x_ec_sb1 = 850, trans_x_ec_sb2 = -1000, trans_y_ec_sb = 40, trans_x_ec_gb = -180, trans_y_ec_gb = 1040,
+	trans_x_chap6, trans_x_title6;
 
 // Variables for Color Morphers
 GLfloat window_top_r = 59, window_top_g = 91, window_top_b = 132,
@@ -2855,10 +2857,301 @@ void workplaceTitleScreen() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Story Title
-	print("CHAPTER 4",
-		1, 1, 1, e, 300, 500 - trans_x_chap4, .2, .2, 2);
-	print("Pre University College",
-		1, 1, 1, puc_title_fade, 400 - trans_x_title4, 450 - trans_x_title4, .3, .3, 2);
+	print("CHAPTER 6",
+		1, 1, 1, wp_chap_fade, 300, 500 - trans_x_chap6, .2, .2, 2);
+	print("Workplace",
+		1, 1, 1, wp_title_fade, 400 - trans_x_title6, 450, .3, .3, 2);
+}
+
+void WP_drawFloor() {
+	glBegin(GL_POLYGON);
+	glColor3ub(80, 80, 80);
+	glVertex2f(0, 75);
+	glVertex2f(1400, 75);
+	glVertex2f(1400, 150);
+	glVertex2f(50, 150);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(0, 75);
+	glVertex2f(1400, 75);
+	glVertex2f(1400, 150);
+	glVertex2f(50, 150);
+	glEnd();
+}
+
+void WP_drawSideWall() {
+	glBegin(GL_POLYGON);
+	glColor3ub(180, 180, 180);
+	glVertex2f(0, 75);
+	glVertex2f(50, 150);
+	glVertex2f(50, 800);
+	glVertex2f(0, 800);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(0, 75);
+	glVertex2f(50, 150);
+	glVertex2f(50, 800);
+	glVertex2f(0, 800);
+	glEnd();
+}
+
+void WP_drawWall() {
+	glBegin(GL_POLYGON);
+	glColor3ub(200, 200, 200);
+	glVertex2f(50, 150);
+	glVertex2f(1400, 150);
+	glVertex2f(1400, 800);
+	glVertex2f(50, 800);
+	glEnd();
+
+	glBegin(GL_LINE_LOOP);
+	glColor3ub(20, 20, 20);
+	glVertex2f(50, 150);
+	glVertex2f(1400, 150);
+	glVertex2f(1400, 800);
+	glVertex2f(50, 800);
+	glEnd();
+}
+
+void WP_drawTable() {
+	// Table - Front Face
+	glBegin(GL_POLYGON);
+	glColor3ub(96, 48, 13);
+	glVertex2f(100, 100);
+	glVertex2f(500, 100);
+	glVertex2f(500, 250);
+	glVertex2f(100, 250);
+	glEnd();
+
+	// Table - Top Face
+	glBegin(GL_POLYGON);
+	glColor3ub(81, 39, 8);
+	glVertex2f(100, 250);
+	glVertex2f(500, 250);
+	glVertex2f(525, 275);
+	glVertex2f(125, 275);
+	glEnd();
+
+	// Table - Side Face
+	glBegin(GL_POLYGON);
+	glColor3ub(58, 28, 6);
+	glVertex2f(500, 100);
+	glVertex2f(525, 150);
+	glVertex2f(525, 275);
+	glVertex2f(500, 250);
+	glEnd();
+}
+
+void WP_drawTechie() {
+	drawCircle(300, 380,
+		203, 166, 108,
+		1, 1.2,
+		30);
+
+	drawSemiCircle(300, 400, 1, .6,
+		20, 20, 20,
+		32, -90, 90);
+	drawSemiCircle(320, 390, .6, .6,
+		20, 20, 20,
+		35, 0, 180);
+
+	// Left Eye
+	drawCircle(298, 383,
+		255, 255, 255,
+		1, 1,
+		5);
+	drawCircle(297, 382,
+		20, 20, 20,
+		1, 1,
+		2);
+
+	// Right Eye
+	drawCircle(278, 383,
+		255, 255, 255,
+		1, 1,
+		5);
+	drawCircle(277, 382,
+		20, 20, 20,
+		1, 1,
+		2);
+
+	// Body
+	drawCircle(300, 275,
+		200, 40, 40,
+		.6, .8,
+		80);
+}
+
+void WP_drawComputer() {
+	// Monitor
+	glBegin(GL_POLYGON);
+	glColor3ub(80, 80, 80);
+	glVertex2f(210, 280);
+	glVertex2f(400, 280);
+	glVertex2f(400, 380);
+	glVertex2f(210, 380);
+	glEnd();
+
+	// Monitor Logo
+	drawCircle(300, 325,
+		40, 40, 40,
+		1, 1,
+		10);
+
+	// Monitor Stand
+	glBegin(GL_POLYGON);
+	glColor3ub(20, 20, 20);
+	glVertex2f(290, 280);
+	glVertex2f(280, 260);
+	glVertex2f(320, 260);
+	glVertex2f(310, 280);
+	glEnd();
+
+	// Mouse
+	drawSemiCircle(240, 262, 1, 1,
+		150, 150, 150,
+		10, -90, 91);
+}
+
+void WP_drawAC() {
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 255, 255);
+	glVertex2f(300, 675);
+	glVertex2f(700, 675);
+	glVertex2f(700, 750);
+	glVertex2f(300, 750);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3ub(240, 240, 240);
+	glVertex2f(300, 675);
+	glVertex2f(310, 660);
+	glVertex2f(710, 660);
+	glVertex2f(700, 675);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3ub(230, 230, 230);
+	glVertex2f(700, 750);
+	glVertex2f(710, 720);
+	glVertex2f(710, 660);
+	glVertex2f(700, 675);
+	glEnd();
+
+	glLineWidth(3);
+	glBegin(GL_LINES);
+	glColor3ub(150, 150, 150);
+	glVertex2f(320, 666);
+	glVertex2f(690, 666);
+	glEnd();
+	glLineWidth(1);
+}
+
+bool server_on;
+
+void WP_drawServer(GLfloat tx, GLfloat ty, bool server_on) {
+	glPushMatrix();
+	glTranslatef(tx, ty, 0);
+
+	// Server
+  glBegin(GL_POLYGON);
+	glColor3ub(30, 30, 30);
+	glVertex2f(960, 460);
+	glVertex2f(1200, 460);
+	glVertex2f(1200, 530);
+	glVertex2f(960, 530);
+	glEnd();
+
+	if (server_on) {
+		drawCircle(990, 490,
+			255, 255, 0,
+			1, 1, 3);
+	}
+
+	glPopMatrix();
+}
+
+void WP_drawServerInnerBox(GLfloat tx, GLfloat ty) {
+	glPushMatrix();
+	glTranslatef(tx, ty, 0);
+
+	// Server Inner Box Back Wall
+	glBegin(GL_POLYGON);
+	glColor3ub(200, 200, 200);
+	glVertex2f(920, 450);
+	glVertex2f(1400, 450);
+	glVertex2f(1400, 580);
+	glVertex2f(920, 580);
+	glEnd();
+
+	// Server Inner Box Side Wall
+	glBegin(GL_POLYGON);
+	glColor3ub(180, 180, 180);
+	glVertex2f(920, 450);
+	glVertex2f(945, 475);
+	glVertex2f(945, 580);
+	glVertex2f(920, 580);
+	glEnd();
+
+	// Server Inner Box Floor
+	glBegin(GL_POLYGON);
+	glColor3ub(160, 160, 160);
+	glVertex2f(920, 450);
+	glVertex2f(1400,450);
+	glVertex2f(1400, 475);
+	glVertex2f(945, 475);
+	glEnd();
+
+	WP_drawServer(0, 0, !server_on);
+	WP_drawServer(280, 0, server_on);
+
+	glPopMatrix();
+}
+
+void WP_drawServerBox() {
+	// Server Box
+	glBegin(GL_POLYGON);
+	glColor3ub(60, 60, 60);
+	glVertex2f(900, 100);
+	glVertex2f(1400, 100);
+	glVertex2f(1400, 600);
+	glVertex2f(900, 600);
+	glEnd();
+
+	// Server Box Top
+	glBegin(GL_POLYGON);
+	glColor3ub(30, 30, 30);
+	glVertex2f(900, 600);
+	glVertex2f(1400, 600);
+	glVertex2f(1400, 610);
+	glVertex2f(925, 610);
+	glEnd();
+
+	WP_drawServerInnerBox(0, 0);
+	WP_drawServerInnerBox(0, -165);
+	WP_drawServerInnerBox(0, -330);
+}
+
+// Workplace
+void workplace() {
+	// Background
+	glClearColor(0.05, 0.05, 0.05, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	WP_drawFloor();
+	WP_drawSideWall();
+	WP_drawWall();
+
+	WP_drawTechie();
+	WP_drawTable();
+	WP_drawComputer();
+
+	WP_drawAC();
+	WP_drawServerBox();
 }
 
 // Function to Render Scene
@@ -2903,6 +3196,9 @@ void renderScene() {
 		break;
 	case 12:
 		workplaceTitleScreen();
+		break;
+	case 13:
+		workplace();
 		break;
 	default:
 		break;
@@ -3024,6 +3320,7 @@ void update(int) {
 	if (SCENE_ID == 6) {
 		if (trans_x_chap3 < 100)
 			trans_x_chap3 += 1;
+
 		if (trans_x_title3 < 100)
 			trans_x_title3 += 1;
 
@@ -3142,6 +3439,7 @@ void update(int) {
 	if (SCENE_ID == 8) {
 		if (trans_x_chap4 < 100)
 			trans_x_chap4 += 1;
+
 		if (trans_x_title4 < 100)
 			trans_x_title4 += 1;
 
@@ -3224,7 +3522,28 @@ void update(int) {
 
 		if (ec_subtitle_1_fade < 1)
 			ec_subtitle_1_fade += 0.005;
+	}
 
+	if (SCENE_ID == 12) {
+		if (trans_x_chap6 < 100)
+			trans_x_chap6 += 1;
+
+		if (trans_x_title6 < 100)
+			trans_x_title6 += 1;
+
+		if (wp_chap_fade < 1)
+			wp_chap_fade += .025;
+
+		if (wp_title_fade < 1)
+			wp_title_fade += .025;
+	}
+
+	if (SCENE_ID == 13) {
+		if (server_on) {
+			server_on = false;
+		} else {
+			server_on = true;
+		}
 	}
 
 	// Recalls the Display Function
